153384eff39597976d7e2c92b72f4781ec265c5d
Thina Mzosindiso Nontwabaza <219189153@mycput.ac.za>
2022-02-06 15:12:39 +0200

    First commit

---
+++ src/main/java/za/ac/cput/App.java
@@ -0,0 +1,37 @@
+package za.ac.cput;
+
+/**
+ *
+ * @author 219189153
+ */
+
+public class App {
+
+    private int num1, num2;
+
+    public App() {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    //Getters and setters
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Calculate {" + "num1=" + num1 + ", num2=" + num2 + '}';
+    }
+}
